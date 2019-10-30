dd2d4562d0ed8aa9eb5d57fdeb3128fd4759e125
daniely-93 <danielyona93@gmail.com>
2019-10-30 21:36:49 +0200

    Added url parameters

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ console.log('Main!');
 
 import locService from './services/loc.service.js'
 import mapService from './services/map.service.js'
+import weatherService from './services/weather.service.js'
+
+
 
 
 locService.getLocs()
@@ -10,22 +13,44 @@ locService.getLocs()
 
 
 window.onload = () => {
-    mapService.initMap()
-        .then(() => {
-            mapService.addMarker({ lat: 32.0749831, lng: 34.9120554 });
-            locService.getLocationName({ lat: 32.0749831, lng: 34.9120554 }).then(data => document.querySelector('.location-text').innerText = data.plus_code.compound_code.substring(8));
-        })
-        .catch(console.log('INIT MAP ERROR'));
+    let lat = getParameterByName('lat');
+    let lng = getParameterByName('lng');
+    if (lat && lng) {
+        mapService.initMap(+lat, +lng)
+            .then(() => {
+                mapService.addMarker({ lat: +lat, lng: +lng });
+                locService.getLocationName({ lat: +lat, lng: +lng })
+                    .then(data => document.querySelector('.location-text').innerText = data.plus_code.compound_code ? data.plus_code.compound_code.substring(8) : 'Invalid location');
+            })
+            .catch(console.log('INIT MAP ERROR'));
+        return;
+    }
 
 
     locService.getPosition()
         .then(pos => {
-            console.log('User position is:', pos.coords);
+            mapService.initMap(pos.coords.latitude, pos.coords.longitude)
+                .then(() => {
+                    mapService.addMarker({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+                    locService.getLocationName({ lat: pos.coords.latitude, lng: pos.coords.longitude }).then(data => document.querySelector('.location-text').innerText = data.plus_code.compound_code.substring(8));
+                })
+                .catch(console.log('INIT MAP ERROR'));
         })
         .catch(err => {
             console.log('err!!!', err);
         })
 }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 
 document.querySelector('.btn-my-location').addEventListener('click', (ev) => {
     locService.getPosition()
@@ -41,6 +66,7 @@ function onSelectPos(pos) {
     mapService.panTo(pos.lat, pos.lng);
     mapService.addMarker({ lat: pos.lat, lng: pos.lng });
     locService.getLocationName({ lat: pos.lat, lng: pos.lng }).then(data => document.querySelector('.location-text').innerText = data.plus_code.compound_code.substring(8));
+    weatherService.getTodayhWeather({ lat: pos.lat, lon: pos.lng }).then(data => console.log)
 }
 
 function onSearch() {
---
+++ js/services/weather.service.js
@@ -0,0 +1,10 @@
+export default {
+    getTodayhWeather
+}
+
+function getTodayhWeather({lat, lon}){
+    return new Promise(resolve => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=2185cb314ffbd44ade56377de3b6ba88`)
+            .then(data => resolve(data.json()));
+    })
+}
